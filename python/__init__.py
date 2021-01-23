4cb1101063a26e65f6d9eef0dd882b49f9decf07
Adam Horden <adam.horden@horden.engineering>
2021-01-23 20:38:17 +0000

    Python: Various Python changes for GNU Radio version 3.9.0.0.
    Signed-off-by: Adam Horden <adam.horden@horden.engineering>

--- python/__init__.py
+++ python/__init__.py
@@ -1,40 +1,20 @@
+# Copyright 2011-2020 Free Software Foundation, Inc.
 #
-# Copyright 2008,2009 Free Software Foundation, Inc.
+# This file is part of GNU Radio
 #
-# This application is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3, or (at your option)
-# any later version.
-#
-# This application is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# SPDX-License-Identifier: GPL-3.0-or-later
 #
 
 # The presence of this file turns this directory into a Python package
 
 '''
-This is the GNU Radio IIO module. Place your Python package
-description here (python/__init__.py).
+This is the GNU Radio IIO module.
 '''
 
-from __future__ import unicode_literals
 import os
 
+# import pybind11 generated symbols into the iio namespace
 try:
-    from .iio_swig import *
-    from .iio_pluto_source_swig import *
-    from .iio_pluto_sink_swig import *
-except ImportError:
-    dirname, filename = os.path.split(os.path.abspath(__file__))
-    __path__.append(os.path.join(dirname, "..", "..", "swig"))
-    from .iio_swig import *
-    from .iio_pluto_source_swig import *
-    from .iio_pluto_sink_swig import *
-
-from .attr_updater import attr_updater
+    from .iio_python import *
+except ModuleNotFoundError:
+    pass
